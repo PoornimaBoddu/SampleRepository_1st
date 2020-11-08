3a8dcd1eb58acb3b36319e500d6f4bd145caef07
PoornimaBoddu <cutepoori@gmail.com>
2020-11-08 23:01:15 +0530

    added modified file

--- addnumbers.java
+++ addnumbers.java
@@ -3,7 +3,7 @@ public class addnumbers {
 	
 	public static void main(String[] args) {
 
-		int a1 = 2, a2 = 3, sum = 0;
+		int a1 = 2, a2 = 6, sum = 0;
 		sum = a1 + a2;
 		System.out.println("sum of two numbers is " + " " + sum);
 	}
